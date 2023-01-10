2b9e7fc0097fb0b4f611581dd80566080d3237a0
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2023-01-10 15:09:28 +0300

    feat: implement exponential smoothing

---
+++ internal/predictor/exponentialSmoothing/exponentialSmoothing.go
@@ -0,0 +1,32 @@
+package exponentialSmoothing
+
+type ExponentialSmoothing struct {
+	alpha, beta float64
+}
+
+func New(alpha, beta float64) *ExponentialSmoothing {
+	return &ExponentialSmoothing{
+		alpha: alpha,
+		beta:  beta,
+	}
+}
+
+func (es *ExponentialSmoothing) PredictForDay(data []float64, day int) (float64, error) {
+	level := []float64{data[0]}
+	trend := []float64{0}
+
+	for i := 1; i < len(data); i++ {
+		l := es.alpha*data[i] + (1-es.alpha)*(level[i-1]+trend[i-1])
+		t := es.beta*(l-level[i-1]) + (1-es.beta)*trend[i-1]
+		level = append(level, l)
+		trend = append(trend, t)
+	}
+
+	futureValues := make([]float64, 0)
+	for i := 0; i < day-len(data)-1; i++ {
+		futureValue := level[len(level)-1] + trend[len(trend)-1]*(float64(i+1))
+		futureValues = append(futureValues, futureValue)
+	}
+
+	return futureValues[len(futureValues)-1], nil
+}
